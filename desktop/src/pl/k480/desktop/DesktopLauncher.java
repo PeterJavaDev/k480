f031d97d5713347339a565557334aa5f6f146e19
PeterJavaDev <delphiman@o2.pl>
2019-03-09 13:32:44 +0100

    initial commit

---
+++ desktop/src/pl/k480/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.k480.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.k480.MainGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGdxGame(), config);
+	}
+}
